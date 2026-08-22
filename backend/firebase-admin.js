b793357fbb79f51937375bd469810b7f4f881396
tyerekimea <tyerekimea@gmail.com>
2026-08-22 14:27:04 +0100

    Fix duplicate getFirestore import in firebase-admin.js

--- backend/firebase-admin.js
+++ backend/firebase-admin.js
@@ -1,7 +1,6 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
 function getServiceAccount() {
